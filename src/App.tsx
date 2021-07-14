9244922ac665d838206abd7afe9589f91f5dd8bc
Ming-Zhao Shi <shim@xceed.com>
2021-07-13 23:50:26 -0400

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 
 function App() {
   return (
-    <ul>
-      {robots.map((r) => (
-        <Robot id={r.id} email={r.email} name={r.name} />
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map((r) => (
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </div>
+    </div>
   );
 }
 
